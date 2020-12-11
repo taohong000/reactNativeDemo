f3128d8e4243e9ecaf6c0b85c4ac23e6ebc076ee
taohong <th19891014@gmail.com>
2020-12-11 16:19:53 +0800

    改变app.js

--- App.js
+++ App.js
@@ -1,116 +1,181 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
+  AppRegistry,
+  Dimensions,
+  Image,
   StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+  TouchableOpacity,
+  View,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import CodePush from 'react-native-code-push';
 
-import codePush from "react-native-code-push"
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {restartAllowed: true};
+  }
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>热推送测试</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-codePush(App)
+  codePushStatusDidChange(syncStatus) {
+    switch (syncStatus) {
+      case CodePush.SyncStatus.CHECKING_FOR_UPDATE:
+        this.setState({syncMessage: 'Checking for update.'});
+        break;
+      case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
+        this.setState({syncMessage: 'Downloading package.'});
+        break;
+      case CodePush.SyncStatus.AWAITING_USER_ACTION:
+        this.setState({syncMessage: 'Awaiting user action.'});
+        break;
+      case CodePush.SyncStatus.INSTALLING_UPDATE:
+        this.setState({syncMessage: 'Installing update.'});
+        break;
+      case CodePush.SyncStatus.UP_TO_DATE:
+        this.setState({syncMessage: 'App up to date.', progress: false});
+        break;
+      case CodePush.SyncStatus.UPDATE_IGNORED:
+        this.setState({
+          syncMessage: 'Update cancelled by user.',
+          progress: false,
+        });
+        break;
+      case CodePush.SyncStatus.UPDATE_INSTALLED:
+        this.setState({
+          syncMessage: 'Update installed and will be applied on restart.',
+          progress: false,
+        });
+        break;
+      case CodePush.SyncStatus.UNKNOWN_ERROR:
+        this.setState({
+          syncMessage: 'An unknown error occurred.',
+          progress: false,
+        });
+        break;
+    }
+  }
+
+  codePushDownloadDidProgress(progress) {
+    this.setState({progress});
+  }
+
+  toggleAllowRestart() {
+    this.state.restartAllowed
+      ? CodePush.disallowRestart()
+      : CodePush.allowRestart();
+
+    this.setState({restartAllowed: !this.state.restartAllowed});
+  }
+
+  getUpdateMetadata() {
+    CodePush.getUpdateMetadata(CodePush.UpdateState.RUNNING).then(
+      (metadata) => {
+        this.setState({
+          syncMessage: metadata
+            ? JSON.stringify(metadata)
+            : 'Running binary version',
+          progress: false,
+        });
+      },
+      (error) => {
+        this.setState({syncMessage: 'Error: ' + error, progress: false});
+      },
+    );
+  }
+
+  /** Update is downloaded silently, and applied on restart (recommended) */
+  sync() {
+    CodePush.sync(
+      {},
+      this.codePushStatusDidChange.bind(this),
+      this.codePushDownloadDidProgress.bind(this),
+    );
+  }
+
+  /** Update pops a confirmation dialog, and then immediately reboots the app */
+  syncImmediate() {
+    CodePush.sync(
+      {installMode: CodePush.InstallMode.IMMEDIATE, updateDialog: true},
+      this.codePushStatusDidChange.bind(this),
+      this.codePushDownloadDidProgress.bind(this),
+    );
+  }
+
+  render() {
+    let progressView;
+
+    if (this.state.progress) {
+      progressView = (
+        <Text style={styles.messages}>
+          {this.state.progress.receivedBytes} of{' '}
+          {this.state.progress.totalBytes} bytes received
+        </Text>
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to CodePush! version 1.0.3 </Text>
+        <TouchableOpacity onPress={this.sync.bind(this)}>
+          <Text style={styles.syncButton}>Press for background sync</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.syncImmediate.bind(this)}>
+          <Text style={styles.syncButton}>Press for dialog-driven sync</Text>
+        </TouchableOpacity>
+        {progressView}
+
+        <TouchableOpacity onPress={this.toggleAllowRestart.bind(this)}>
+          <Text style={styles.restartToggleButton}>
+            Restart {this.state.restartAllowed ? 'allowed' : 'forbidden'}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.getUpdateMetadata.bind(this)}>
+          <Text style={styles.syncButton}>Press for Update Metadata</Text>
+        </TouchableOpacity>
+        <Text style={styles.messages}>{this.state.syncMessage || ''}</Text>
+      </View>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    paddingTop: 50,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  image: {
+    margin: 30,
+    width: Dimensions.get('window').width - 100,
+    height: (365 * (Dimensions.get('window').width - 100)) / 651,
   },
-  body: {
-    backgroundColor: Colors.white,
+  messages: {
+    marginTop: 30,
+    textAlign: 'center',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  restartToggleButton: {
+    color: 'blue',
+    fontSize: 17,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  syncButton: {
+    color: 'green',
+    fontSize: 17,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 20,
   },
 });
+
+/**
+ * Configured with a MANUAL check frequency for easy testing. For production apps, it is recommended to configure a
+ * different check frequency, such as ON_APP_START, for a 'hands-off' approach where CodePush.sync() does not
+ * need to be explicitly called. All options of CodePush.sync() are also available in this decorator.
+ */
+let codePushOptions = {checkFrequency: CodePush.CheckFrequency.MANUAL};
+
+App = CodePush(codePushOptions)(App);
 
 export default App;
